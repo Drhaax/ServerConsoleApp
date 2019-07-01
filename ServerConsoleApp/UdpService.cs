60937a47c271b375b33ce4b5682a62c1744d637e
mvitikainen <45352875+mvitikainen@users.noreply.github.com>
2019-07-01 16:46:38 +0300

    version 2

---
+++ ServerConsoleApp/UdpService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using ServerConsoleApp;
+
+public class UdpService
+{
+    private UdpSocket client;
+
+    public UdpService(int targetPort, string targetHost) {
+        //for client
+        client = new UdpSocket(new IPEndPoint(IPAddress.Parse(targetHost), targetPort));
+    }
+    public UdpService(int bindPort, int targetPort) {
+        //for serverside
+        client = new UdpSocket(new IPEndPoint(IPAddress.Any, targetPort), bindPort);
+    }
+    internal void StartService() {
+        client.ListenForMessages(OnReceive);
+    }
+
+    private void OnSend(IAsyncResult ar) {
+        var aaa = (UdpState)ar.AsyncState;
+        Print.Line("Sent " + aaa.dataStream.ToString());
+
+    }
+
+    void OnReceive(IAsyncResult ar) {
+        var s = (UdpState)ar.AsyncState;
+        Print.Line("receive" + s.dataStream.ToString());
+        var bytes = s.client.EndReceive(ar, ref s.dataStream);
+        var msg = new Message(bytes);
+
+        switch (msg.type) {
+            case MessageType.None:
+            break;
+            case MessageType.LogIn:
+            if (Login != null) {
+                Login.Invoke(msg, s);
+            }
+            break;
+            case MessageType.LogOut:
+            if (Logout != null) {
+                Logout.Invoke(msg, s);
+            }
+            break;
+            case MessageType.Movement:
+            if (Movement != null) {
+                Movement.Invoke(msg, s);
+            }
+            break;
+        }
+        client.Receive(OnReceive);
+    }
+    ///<summary>Send message to specific "client" using UdpState</summary>
+    public void SendMessage(Message msg, UdpState s) {
+        client.Send(msg, OnSend, s);
+    }
+    ///<summary>Send message to "default client" without specifying UdpState</summary>
+    public void SendMessage(Message msg) {
+        client.Send(msg, OnSend);
+    }
+    Action<Message, UdpState> Login;
+    Action<Message, UdpState> Logout;
+    Action<Message, UdpState> Movement;
+    public void AddMessageCallback(Action<Message, UdpState> Login, Action<Message, UdpState> Logout, Action<Message, UdpState> Movement) {
+        this.Login = Login;
+        this.Logout = Logout;
+        this.Movement = Movement;
+    }
+}
--- ServerConsoleApp/UdpSocket.cs
+++ ServerConsoleApp/UdpSocket.cs
@@ -2,51 +2,69 @@
 using System.Net;
 using System.Net.Sockets;
 
-namespace ServerConsoleApp {
+namespace ServerConsoleApp
+{
 
-	public struct UdpState {
-		public UdpClient client;
-		public IPEndPoint dataStream;
-	}
-	public class UdpSocket {
+    public struct UdpState
+    {
+        public UdpClient client;
+        public IPEndPoint dataStream;
+    }
+    public class UdpSocket
+    {
 
-		private bool running;
-		public UdpClient socket;
+        private bool running;
+        public UdpClient socket;
+        public UdpState socketState;
+        public UdpSocket(IPEndPoint target, int port) {
+            socket = new UdpClient(port);
 
-		public UdpSocket(int port) {
-			socket = new UdpClient(port);
-		}
-		public UdpSocket() {
-			socket = new UdpClient();
-		}
-		#region public methods
-		public void Receive(AsyncCallback callback, UdpState state) {
-			socket.BeginReceive(callback, state);
-		}
-		public void Send(Message msg, AsyncCallback callback, IPEndPoint target, UdpState state) {
-			socket.BeginSend(msg.GetBytes(), msg.GetBytes().Length, target, callback, state);
-		}
-		public void Shutdown() {
-			running = false;
-			socket.Close();
-			Environment.Exit(0);
-		}
+            this.socketState = new UdpState() {
+                client = socket,
+                dataStream = target
+            };
 
-		public void ListenForMessages(AsyncCallback callback, UdpState state) {
-			if (running) {
-				Console.WriteLine("Already Running!");
-				return;
-			}
-			try {
-				Receive(callback, state);
-				running = true;
-			} catch (Exception e) {
-				Console.WriteLine(e);
-			}
+        }
+        public UdpSocket(IPEndPoint target) {
+            socket = new UdpClient();
 
-		}
-		#endregion
+            this.socketState = new UdpState() {
+                client = socket,
+                dataStream = target
+            };
 
-	}
+        }
+        #region public methods
+        public void Receive(AsyncCallback callback) {
+            socket.BeginReceive(callback, socketState);
+        }
+        public void Send(Message msg, AsyncCallback callback, UdpState s) {
+            socket.BeginSend(msg.GetBytes(), msg.GetBytes().Length, s.dataStream, callback, s);
+        }
+        public void Send(Message msg, AsyncCallback callback) {
+            socket.BeginSend(msg.GetBytes(), msg.GetBytes().Length, socketState.dataStream, callback, socketState);
+        }
+        public void Shutdown() {
+            running = false;
+            socket.Close();
+            Environment.Exit(0);
+        }
+
+        public void ListenForMessages(AsyncCallback callback) {
+            if (running) {
+                Console.WriteLine("Already Running!");
+                return;
+            }
+            try {
+                Receive(callback);
+                running = true;
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+        }
+        #endregion
+
+    }
 
 }
